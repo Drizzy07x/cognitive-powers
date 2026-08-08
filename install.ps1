2e8935a499ee5979289362c8265d22d76c4d0033
Drizzy07x <42722790+Drizzy07x@users.noreply.github.com>
2026-08-08 14:11:15 -0400

    chore: declare 1.10.0-rc.4 and move every carrier to it

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 [CmdletBinding()]
 param(
     [ValidatePattern('^v\d+\.\d+\.\d+(-(alpha|beta|rc)\.\d+)?$')]
-    [string]$ReleaseRef = "v1.10.0-rc.3",
+    [string]$ReleaseRef = "v1.10.0-rc.4",
     [switch]$Help
 )
 
